38e7f1e844733b68051935db6f35b2fe802e155d
Daniil Ivanov <d.a.ivanov@comagic.dev>
2023-05-01 18:58:59 +0300

    add modal, portal, change header

---
+++ src/shared/ui/Portal/Portal.tsx
@@ -0,0 +1,20 @@
+import { FC, ReactNode } from 'react';
+import { createPortal } from 'react-dom';
+
+interface PortalProps {
+    element?: HTMLElement;
+    children: ReactNode;
+}
+
+const Portal: FC<PortalProps> = (props) => {
+    const {
+        element = document.body,
+        children,
+    } = props;
+
+    return (
+        createPortal(children, element)
+    );
+};
+
+export default Portal;
--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,5 +1,8 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 import { classNames } from 'shared/lib/classNames/classNames';
+import { useTranslation } from 'react-i18next';
+import { Modal } from 'shared/ui/Modal/Modal';
+import Button, { ButtonTheme } from 'shared/ui/Button/Button';
 import cls from './Navbar.module.scss';
 
 interface NavbarProps {
@@ -11,11 +14,28 @@ const Navbar: FC<NavbarProps> = (props) => {
         className,
     } = props;
 
+    const { t } = useTranslation();
+
+    const [isAuthModal, setIsAuthModal] = useState(false);
+
+    const onToggleModal = () => {
+        setIsAuthModal((prev) => !prev);
+    };
+
     return (
         <div className={classNames(cls.Navbar, {}, [className])}>
-            <div className={cls.links}>
-                /
-            </div>
+            <Button
+                className={cls.links}
+                theme={ButtonTheme.CLEAR_INVERTED}
+                onClick={onToggleModal}
+            >
+                {t('Войти')}
+            </Button>
+
+            <Modal isOpen={isAuthModal} onClose={onToggleModal}>
+                {/* eslint-disable-next-line */}
+                {t('Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis consectetur delectus eos incidunt mollitia nostrum perspiciatis quia quisquam reiciendis voluptatum.')}
+            </Modal>
         </div>
     );
 };
