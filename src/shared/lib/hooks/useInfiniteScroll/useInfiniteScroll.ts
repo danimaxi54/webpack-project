0564332c51f6b20363847dd4256bee5625feee9f
Daniil Ivanov <d.a.ivanov@comagic.dev>
2023-06-20 14:07:56 +0300

    fix infinite scroll error while dismantling

--- src/shared/lib/hooks/useInfiniteScroll/useInfiniteScroll.ts
+++ src/shared/lib/hooks/useInfiniteScroll/useInfiniteScroll.ts
@@ -10,9 +10,12 @@ export function useInfiniteScroll({ callback, wrapperRef, triggerRef }: UseInfin
     const observer = useRef<IntersectionObserver | null>(null);
 
     useEffect(() => {
+        const wrapperElement = wrapperRef.current;
+        const triggerElement = triggerRef.current;
+
         if (callback) {
             const options = {
-                root: wrapperRef.current,
+                root: wrapperElement,
                 rootMargin: '0px',
                 threshold: 1.0,
             };
@@ -23,14 +26,13 @@ export function useInfiniteScroll({ callback, wrapperRef, triggerRef }: UseInfin
                 }
             }, options);
 
-            observer.current.observe(triggerRef.current);
+            observer.current.observe(triggerElement);
         }
 
         return () => {
-            if (observer.current) {
-                // eslint-disable-next-line react-hooks/exhaustive-deps
-                observer.current.unobserve(triggerRef.current);
+            if (observer.current && triggerElement) {
+                observer.current.unobserve(triggerElement);
             }
         };
-    }, [callback, triggerRef, wrapperRef]);
+    }, [triggerRef, wrapperRef]);
 }
