e913d78ceda4d220baacd8d22a2f1c9cefba027e
Daniil Ivanov <d.a.ivanov@comagic.dev>
2023-06-29 00:57:46 +0300

    fix api config

--- src/shared/api/api.ts
+++ src/shared/api/api.ts
@@ -11,4 +11,6 @@ $api.interceptors.request.use((config) => {
     if (config.headers) {
         config.headers.Authorization = localStorage.getItem(USER_LOCALSTORAGE_KEY) || '';
     }
+
+    return config;
 });
