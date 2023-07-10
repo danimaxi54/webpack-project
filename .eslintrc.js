da990c935649e154837df56b76f1e8bbdd2c751d
Daniil Ivanov <d.a.ivanov@comagic.dev>
2023-07-10 15:59:30 +0300

    update custom eslint plugin, add support for aliases

--- .eslintrc.js
+++ .eslintrc.js
@@ -70,7 +70,7 @@ module.exports = {
         'react/jsx-no-useless-fragment': 'off',
         'no-undef': 'off',
         'react/no-array-index-key': 'off',
-        'danimaxi54-plugin-new/path-checker': 'error',
+        'danimaxi54-plugin-new/path-checker': ['error', { alias: '@' }],
     },
     globals: {
         __IS_DEV__: true,
