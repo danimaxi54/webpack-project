aaafa1f434abaa86fe9dd8c7aa61f8e027adbb1a
Daniil Ivanov <d.a.ivanov@comagic.dev>
2023-07-14 22:11:11 +0300

    upgrade storybook

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,106 +1,81 @@
 module.exports = {
-    env: {
-        browser: true,
-        es2021: true,
-        jest: true,
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true
+  },
+  extends: ['plugin:react/recommended', 'airbnb', 'plugin:i18next/recommended', 'plugin:storybook/recommended'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
     },
-    extends: [
-        'plugin:react/recommended',
-        'airbnb',
-        'plugin:i18next/recommended',
-    ],
-    parser: '@typescript-eslint/parser',
-    parserOptions: {
-        ecmaFeatures: {
-            jsx: true,
-        },
-        ecmaVersion: 'latest',
-        sourceType: 'module',
-    },
-    plugins: [
-        'react',
-        '@typescript-eslint',
-        'i18next',
-        'react-hooks',
-        'danimaxi54-plugin-new',
-        'unused-imports',
-    ],
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: ['react', '@typescript-eslint', 'i18next', 'react-hooks', 'danimaxi54-plugin-new', 'unused-imports'],
+  rules: {
+    'react/jsx-indent': [2, 4],
+    'react/jsx-indent-props': [2, 4],
+    indent: [2, 4],
+    'react/jsx-filename-extension': [2, {
+      extensions: ['.js', '.jsx', '.tsx']
+    }],
+    'import/no-unresolved': 'off',
+    'import/prefer-default-export': 'off',
+    'no-unused-vars': 'warn',
+    'react/require-default-props': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-props-no-spreading': 'warn',
+    'react/function-component-definition': 'off',
+    'no-shadow': 'off',
+    'import/extensions': 'off',
+    'import/no-extraneous-dependencies': 'off',
+    'no-underscore-dangle': 'off',
+    'i18next/no-literal-string': ['error', {
+      markupOnly: true,
+      ignoreAttribute: ['role', 'data-testid', 'to', 'target', 'justify', 'align', 'direction', 'gap', 'as', 'border']
+    }],
+    'max-len': ['error', {
+      ignoreComments: true,
+      code: 120
+    }],
+    'jsx-a11y/no-static-element-interactions': 'off',
+    'jsx-a11y/click-events-have-key-events': 'off',
+    'react-hooks/rules-of-hooks': 'error',
+    // Checks rules of Hooks
+    'react-hooks/exhaustive-deps': 'warn',
+    // Checks effect dependencies,
+    'no-param-reassign': 'off',
+    'react/jsx-no-useless-fragment': 'off',
+    'no-undef': 'off',
+    'react/no-array-index-key': 'off',
+    'danimaxi54-plugin-new/path-checker': ['error', {
+      alias: '@'
+    }],
+    'danimaxi54-plugin-new/public-api-imports': ['error', {
+      alias: '@',
+      testFilesPatterns: ['**/*.test.*', '**/*.story.*', '**/StoreDecorator.tsx']
+    }],
+    'danimaxi54-plugin-new/layer-imports': ['error', {
+      alias: '@',
+      ignoreImportPatterns: ['**/StoreProvider', '**/testing']
+    }],
+    'unused-imports/no-unused-imports': 'error',
+    'react/jsx-newline': ['error', {
+      prevent: false
+    }]
+  },
+  globals: {
+    __IS_DEV__: true,
+    __API__: true,
+    __PROJECT__: true
+  },
+  overrides: [{
+    files: ['**/src/**/*.{test,stories}.{ts,tsx}'],
     rules: {
-        'react/jsx-indent': [2, 4],
-        'react/jsx-indent-props': [2, 4],
-        indent: [2, 4],
-        'react/jsx-filename-extension': [
-            2,
-            { extensions: ['.js', '.jsx', '.tsx'] },
-        ],
-        'import/no-unresolved': 'off',
-        'import/prefer-default-export': 'off',
-        'no-unused-vars': 'warn',
-        'react/require-default-props': 'off',
-        'react/react-in-jsx-scope': 'off',
-        'react/jsx-props-no-spreading': 'warn',
-        'react/function-component-definition': 'off',
-        'no-shadow': 'off',
-        'import/extensions': 'off',
-        'import/no-extraneous-dependencies': 'off',
-        'no-underscore-dangle': 'off',
-        'i18next/no-literal-string': [
-            'error',
-            {
-                markupOnly: true,
-                ignoreAttribute: [
-                    'role',
-                    'data-testid',
-                    'to',
-                    'target',
-                    'justify',
-                    'align',
-                    'direction',
-                    'gap',
-                    'as',
-                    'border',
-                ],
-            },
-        ],
-        'max-len': ['error', { ignoreComments: true, code: 120 }],
-        'jsx-a11y/no-static-element-interactions': 'off',
-        'jsx-a11y/click-events-have-key-events': 'off',
-        'react-hooks/rules-of-hooks': 'error', // Checks rules of Hooks
-        'react-hooks/exhaustive-deps': 'warn', // Checks effect dependencies,
-        'no-param-reassign': 'off',
-        'react/jsx-no-useless-fragment': 'off',
-        'no-undef': 'off',
-        'react/no-array-index-key': 'off',
-        'danimaxi54-plugin-new/path-checker': ['error', { alias: '@' }],
-        'danimaxi54-plugin-new/public-api-imports': [
-            'error',
-            {
-                alias: '@',
-                testFilesPatterns: ['**/*.test.*', '**/*.story.*', '**/StoreDecorator.tsx'],
-            },
-        ],
-        'danimaxi54-plugin-new/layer-imports': [
-            'error',
-            {
-                alias: '@',
-                ignoreImportPatterns: ['**/StoreProvider', '**/testing'],
-            },
-        ],
-        'unused-imports/no-unused-imports': 'error',
-        'react/jsx-newline': ['error', { prevent: false }],
-    },
-    globals: {
-        __IS_DEV__: true,
-        __API__: true,
-        __PROJECT__: true,
-    },
-    overrides: [
-        {
-            files: ['**/src/**/*.{test,stories}.{ts,tsx}'],
-            rules: {
-                'i18next/no-literal-string': 'off',
-                'max-len': 'off',
-            },
-        },
-    ],
+      'i18next/no-literal-string': 'off',
+      'max-len': 'off'
+    }
+  }]
 };
